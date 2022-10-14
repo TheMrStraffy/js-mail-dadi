ef79ec80525ce3d437c9537846c4eaf5b5bfa85a
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-10-14 17:36:29 +0200

    [added] dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,27 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+const result = document.querySelector('.result');
+const winner = document.createElement('p');
+let giocatore = document.querySelector('[name="giocatore"]');
+let computer = document.querySelector('[name="computer"]');
+const button = document.querySelector('button');
+let numGenerator1 = Math.ceil(Math.random() * 6);
+let numGenerator2 = Math.ceil(Math.random() * 6);
+
+button.addEventListener('click', function(){
+  
+  giocatore.value;
+  computer.value;
+  giocatore.value = numGenerator1;
+  computer.value = numGenerator2;
+  // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+  if (giocatore.value > computer.value){
+    winner.innerHTML= "Giocatore ha Vinto!"
+  } else if (giocatore.value === computer.value){
+    winner.innerHTML= "PAREGGIOOOOO"
+  } else {
+    winner.innerHTML= "Il computer ha vinto!"
+  }
+  result.append(winner)
+})
+
+
